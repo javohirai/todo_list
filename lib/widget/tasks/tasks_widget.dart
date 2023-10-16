d0ab9d8782af18644ca7c5ec9d61c435feb15fbd
javohirai <javohir8620@gmail.com>
2023-10-16 14:13:43 +0500

    modify tasks model

--- lib/widget/tasks/tasks_widget.dart
+++ lib/widget/tasks/tasks_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/widget/tasks/tasks_widget_model.dart';
 
 class TasksWidget extends StatefulWidget {
   const TasksWidget({super.key});
@@ -8,6 +9,37 @@ class TasksWidget extends StatefulWidget {
 }
 
 class TasksWidgetState extends State<TasksWidget> {
+  TasksWidgetModel? _model;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (_model == null) {
+      int groupKey = ModalRoute.of(context)?.settings.arguments as int;
+      _model = TasksWidgetModel(groupKey: groupKey);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final model = _model;
+    if (model != null) {
+      return TasksWidgetModelProvider(
+        model: model,
+        child: const _TasksWidgetBody(),
+      );
+    } else {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+  }
+}
+
+class _TasksWidgetBody extends StatelessWidget {
+  const _TasksWidgetBody({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container();
---
+++ lib/widget/tasks/tasks_widget_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class TasksWidgetModel extends ChangeNotifier {
+  int groupKey;
+
+  TasksWidgetModel({required this.groupKey});
+  
+}
+
+class TasksWidgetModelProvider extends InheritedNotifier {
+  final TasksWidgetModel model;
+  const TasksWidgetModelProvider({
+    Key? key,
+    required this.model,
+    required Widget child,
+  }) : super(
+          key: key,
+          notifier: model,
+          child: child,
+        );
+  static TasksWidgetModelProvider? watch(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<TasksWidgetModelProvider>();
+  }
+
+  static TasksWidgetModelProvider? read(BuildContext context) {
+    final widget = context
+        .getElementForInheritedWidgetOfExactType<TasksWidgetModelProvider>()
+        ?.widget;
+    return widget is TasksWidgetModelProvider ? widget : null;
+  }
+}
