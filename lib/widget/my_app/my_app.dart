87c5c2222d261169a7a4bcab84b62c01b37008fc
javohirai <javohir8620@gmail.com>
2023-10-16 09:22:34 +0500

    initial commit

---
+++ lib/widget/my_app/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/widget/example/example.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ExampleWidget(),
+    );
+  }
+}
