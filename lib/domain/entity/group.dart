3c5d57fc14a4f667006d304c1221186b482594c5
javohirai <javohir8620@gmail.com>
2023-10-16 15:00:43 +0500

    modify taks form model

--- lib/domain/entity/group.dart
+++ lib/domain/entity/group.dart
@@ -1,5 +1,6 @@
 
 import 'package:hive/hive.dart';
+import 'package:todo_list/domain/entity/task.dart';
 
 part 'group.g.dart';
 
@@ -9,9 +10,14 @@ class Group{
   String name;
 
   @HiveField(1)
-  HiveList? tasks;
+  HiveList<Task>? tasks;
 
   Group({
     required this.name,
   });
+
+  void addTask(Box<Task> box, Task task){
+    tasks ??= HiveList(box);
+    tasks?.add(task);
+  }
 }
